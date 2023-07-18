b779e08e3b409b8f5a6ed6d68fe5f6d8b037d564
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-07-18 21:23:48 +1200

    init

---
+++ car-nav/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CarSearch, CarForm, CarList, CarValue } from './components/Car'
+
+function App() {
+  return (
+    <>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </>
+  )
+}
+
+export default App
---
+++ car-nav/src/components/Car/index.js
@@ -0,0 +1,4 @@
+export { default as CarList } from './CarList'
+export { default as CarForm } from './CarForm'
+export { default as CarValue } from './CarValue'
+export { default as CarSearch } from './CarSearch'
